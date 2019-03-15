163a29497e8db18a4d575fc6c5617c8ea8c7933f
madeirak <haoran4@126.com>
2019-03-15 17:17:23 +0800

    edit

--- general_function/file_dict.py
+++ general_function/file_dict.py
@@ -6,24 +6,26 @@
 
 
 def GetSymbolList(datapath):
-	'''
+    '''
 	加载拼音符号列表，用于标记符号
 	返回一个列表list类型变量
 	'''
-	if(datapath != ''):
-		if(datapath[-1]!='/' or datapath[-1]!='\\'):
-			datapath = datapath + '/'
-	
-	txt_obj=open(datapath + 'dict.txt','r',encoding='UTF-8') # 打开文件并读入   dict.txt是pny2hanzi字典
-	txt_text=txt_obj.read()        #read()读取整个文件
-	txt_lines=txt_text.split('\n') # 文本分割    #split返回字符串列表    分隔每个拼音
-	list_symbol=[] # 初始化符号列表
-	for i in txt_lines:
-		if(i!=''):
-			txt_l=i.split('\t')						#分隔拼音和多音字
-			list_symbol.append(txt_l[0])            #提取拼音
-	txt_obj.close()
-	list_symbol.append('_')
-	#SymbolNum = len(list_symbol)
-	return list_symbol
-	
+
+    datapath_ = datapath.strip('dataset\\')
+    if (datapath_ != ''):
+        if (datapath_[-1] != '/' and datapath_[-1] != '\\'):
+            datapath_ = datapath_ + '/'
+
+    txt_obj = open(datapath_ + 'dict.txt', 'r', encoding='UTF-8') # 打开文件并读入
+    txt_text = txt_obj.read()
+    txt_lines = txt_text.split('\n') # 文本分割
+    list_symbol = [] # 初始化符号列表
+    for i in txt_lines:
+        if (i != ''):
+            txt_l = i.split('\t')
+            list_symbol.append(txt_l[0])
+    txt_obj.close()
+    list_symbol.append('_')
+    # SymbolNum = len(list_symbol)
+    return list_symbol
+
